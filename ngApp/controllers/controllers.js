316018542980ae8a7dd0e5f8e6f8fa58cfe79c5c
John Chleborad <jchleborad@gmail.com>
2018-04-01 17:06:23 -0700

    first commit

---
+++ ngApp/controllers/controllers.js
@@ -0,0 +1,16 @@
+export class HomeController {
+  constructor() {
+  }
+}
+
+export class AboutController {
+  constructor() {
+    this.message = 'about page';
+  }
+}
+
+export class ResumeController {
+  constructor() {
+    this.message = 'Resume';
+  }
+}
